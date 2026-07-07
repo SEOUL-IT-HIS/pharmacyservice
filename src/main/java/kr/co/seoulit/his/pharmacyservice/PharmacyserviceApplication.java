1def4611dc7b517a638b6a5ee8eceb23996ef66a
Crunex-0 <porbez5@gmail.com>
2026-07-07 11:36:05 +0900

    commit

---
+++ src/main/java/kr/co/seoulit/his/pharmacyservice/PharmacyserviceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.seoulit.his.pharmacyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/seoulit/his/pharmacyservice/PharmacyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.seoulit.his.pharmacyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
